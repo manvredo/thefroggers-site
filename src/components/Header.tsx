e51d74af9cf4ca564c7166d2cbd25ecdda9bd851
manvredo <47196172+manvredo@users.noreply.github.com>
2025-08-09 19:48:37 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -61,7 +61,7 @@ export const Header = () => {
         data-border="rounded"
       >
         <Flex paddingLeft="12" fillWidth vertical="center" textVariant="body-default-s">
-          {display.location && <Flex hide="s">{person.location}</Flex>}
+          {/* Location text removed */}
         </Flex>
         <Flex fillWidth horizontal="center">
           <Flex
