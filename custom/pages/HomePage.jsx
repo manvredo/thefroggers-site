b7565d2e0e7ee679eebda66008219477373974b4
manvredo <47196172+manvredo@users.noreply.github.com>
2025-08-10 09:11:45 +0200

    Update HomePage.jsx

--- custom/pages/HomePage.jsx
+++ custom/pages/HomePage.jsx
@@ -1,1 +1,16 @@
 
+// custom/pages/HomePage.jsx
+'use client';
+
+import React from 'react';
+// Später können wir hier Styles importieren
+// import '../styles/main.scss';
+
+export default function HomePage() {
+  return (
+    <div className="custom-homepage">
+      <h1>theFroggers</h1>
+      <p>Leere Basis-Seite zum Aufbau</p>
+    </div>
+  );
+}
