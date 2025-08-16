b545f254958d3102bbc7bbbcd8abad9bb9853e00
manvredo <47196172+manvredo@users.noreply.github.com>
2025-08-16 10:02:27 +0200

    Add files via upload

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -52,23 +52,61 @@ export default function About() {
   ];
   
   return (
-    <Column maxWidth="m" style={{ position: "relative" }}>
-      {/* TheFroggers Logo */}
-      <div
-        style={{
-          position: "absolute",
-          left: "1245px",
-          top: "119px",
-          width: "280.41px",
-          height: "75.15px",
-          backgroundImage: "url('/images/Logo_01 copy.png')",
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          opacity: 0.21,
-          zIndex: 1,
-          pointerEvents: "none"
-        }}
-      />
+    <div className={styles.container}>
+      <div className={styles.desktop}>
+        {/* Background Container */}
+        <div className={styles.bgContainer}></div>
+        
+        {/* Frogger Bubble */}
+        <div className={styles.froggerBubble}></div>
+        
+        {/* Text Elements */}
+        <div className={styles.textRibbit}>
+          We are the bridge between the real and digital worlds, where our stories continue to evolve and new adventures await.
+        </div>
+        <div className={styles.textFroggers}>
+          TheFroggers is a collection of 10,000 unique NFTs living in the vibrant world of Frogtopia.
+        </div>
+        <div className={styles.textMetaverse}>
+          Join us in exploring the infinite possibilities of the metaverse, where creativity knows no bounds.
+        </div>
+        
+        {/* Social Icons */}
+        <a href="https://youtube.com/@thefroggers" className={styles.youtube}></a>
+        <a href="https://twitter.com/thefroggers" className={styles.twitter}></a>
+        <a href="https://instagram.com/thefroggers.io" className={styles.instagram}></a>
+        
+        {/* Logo with Video */}
+        <div className={styles.logoHalo}>
+          <video className={styles.logoVideo} autoPlay muted loop>
+            <source src="/videos/logo-animation.mp4" type="video/mp4" />
+          </video>
+        </div>
+        
+        {/* Frogger Images */}
+        <div className={styles.froggerAbout}></div>
+        <div className={styles.tenThousandFroggers}></div>
+        <div className={styles.froggersText}>Froggers</div>
+        <div className={styles.aboutBalloon}></div>
+        <div className={styles.ballonYellowFrogger}></div>
+        
+        {/* TheFroggers Logo an der gewünschten Position */}
+        <div
+          style={{
+            position: "absolute",
+            left: "1245px",
+            top: "119px",
+            width: "280.41px",
+            height: "75.15px",
+            backgroundImage: "url('/images/Logo_01 copy.png')",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            opacity: 0.21,
+            zIndex: 1,
+            pointerEvents: "none"
+          }}
+        />
+      </div>
       
       <Schema
         as="webPage"
@@ -83,204 +121,6 @@ export default function About() {
           image: `${baseURL}${person.avatar}`,
         }}
       />
-      {about.tableOfContent.display && (
-        <Column
-          left="0"
-          style={{ top: "50%", transform: "translateY(-50%)" }}
-          position="fixed"
-          paddingLeft="24"
-          gap="32"
-          hide="s"
-        >
-          <TableOfContents structure={structure} about={about} />
-        </Column>
-      )}
-      <Flex fillWidth mobileDirection="column" horizontal="center" style={{ position: "relative", zIndex: 2 }}>
-        {about.avatar.display && (
-          <Column
-            className={styles.avatar}
-            position="sticky"
-            minWidth="160"
-            paddingX="l"
-            paddingTop="40"
-            gap="m"
-          >
-            <Avatar
-              src={person.avatar}
-              size="xl"
-            />
-            {about.calendar.display && (
-              <Button
-                href={about.calendar.link}
-                prefixIcon="calendar"
-                label="Follow"
-                size="s"
-                weight="default"
-                variant="secondary"
-              />
-            )}
-            {social.length > 0 && (
-              <Flex fillWidth gap="8" wrap>
-                {social.map(
-                  (item) =>
-                    item.link && (
-                        <React.Fragment key={item.name}>
-                            <Button
-                                className="s-flex-hide"
-                                key={item.name}
-                                href={item.link}
-                                prefixIcon={item.icon}
-                                label={item.name}
-                                size="s"
-                                weight="default"
-                                variant="secondary"
-                            />
-                            <IconButton
-                                className="s-flex-show"
-                                size="l"
-                                key={`${item.name}-icon`}
-                                href={item.link}
-                                icon={item.icon}
-                                variant="secondary"
-                            />
-                        </React.Fragment>
-                    ),
-                )}
-              </Flex>
-            )}
-          </Column>
-        )}
-
-        <Column fillWidth maxWidth="s" gap="l" className={styles.textAlign}>
-          {about.intro.display && (
-            <Column textVariant="body-default-l" fillWidth gap="m" marginBottom="xl">
-              {about.intro.description}
-            </Column>
-          )}
-
-          {about.work.display && (
-            <>
-              <Heading as="h2" id={about.work.title} variant="display-strong-s" marginBottom="m">
-                {about.work.title}
-              </Heading>
-              <Column fillWidth gap="l" marginBottom="40">
-                {about.work.experiences.map((experience, index) => (
-                  <Column key={`${experience.company}-${experience.role}-${index}`} fillWidth>
-                    <Flex fillWidth horizontal="space-between" vertical="end" marginBottom="4">
-                      <Text id={experience.company} variant="heading-strong-l">
-                        {experience.company}
-                      </Text>
-                      <Text variant="heading-default-xs" onBackground="neutral-weak">
-                        {experience.timeframe}
-                      </Text>
-                    </Flex>
-                    <Text variant="body-default-s" onBackground="brand-weak" marginBottom="m">
-                      {experience.role}
-                    </Text>
-                    <Column gap="4">
-                      {experience.achievements && experience.achievements.map((achievement, index) => (
-                        <Text key={index} variant="body-default-m" onBackground="neutral-weak">
-                          {achievement}
-                        </Text>
-                      ))}
-                    </Column>
-                    {experience.images && experience.images.length > 0 && (
-                      <Flex fillWidth paddingTop="m" gap="12" wrap>
-                        {experience.images.map((image, index) => (
-                          <Flex
-                            key={index}
-                            border="neutral-medium"
-                            radius="m"
-                            minWidth={image.width}
-                            height={image.height}
-                          >
-                            <Media
-                              enlarge
-                              radius="m"
-                              sizes={image.width.toString()}
-                              alt={image.alt}
-                              src={image.src}
-                            />
-                          </Flex>
-                        ))}
-                      </Flex>
-                    )}
-                  </Column>
-                ))}
-              </Column>
-            </>
-          )}
-
-          {about.studies.display && (
-            <>
-              <Heading
-                as="h2"
-                id={about.studies.title}
-                variant="display-strong-s"
-                marginBottom="40"
-              >
-                {about.studies.title}
-              </Heading>
-              <Column fillWidth gap="l">
-                {about.studies.institutions.map((institution, index) => (
-                  <Column key={`${institution.name}-${index}`} fillWidth gap="8">
-                    <Text id={institution.name} variant="heading-strong-l">
-                      {institution.name}
-                    </Text>
-                    <Text variant="heading-default-xs" onBackground="neutral-weak">
-                      {institution.description}
-                    </Text>
-                  </Column>
-                ))}
-              </Column>
-            </>
-          )}
-
-          {about.technical.display && (
-            <>
-              <Heading
-                as="h2"
-                id={about.technical.title}
-                variant="display-strong-s"
-                marginBottom="40"
-              >
-                {about.technical.title}
-              </Heading>
-              <Column fillWidth gap="l">
-                {about.technical.skills.map((skill, index) => (
-                  <Column key={`${skill}-${index}`} fillWidth gap="4">
-                    <Text id={skill.title} variant="heading-strong-l">{skill.title}</Text>
-                    <Text variant="body-default-m" onBackground="neutral-weak">
-                      {skill.description}
-                    </Text>
-                    {skill.images && skill.images.length > 0 && (
-                      <Flex fillWidth paddingTop="m" gap="12" wrap>
-                        {skill.images.map((image, index) => (
-                          <Flex
-                            key={index}
-                            border="neutral-medium"
-                            radius="m"
-                            minWidth={image.width}
-                            height={image.height}
-                          >
-                            <Media
-                              enlarge
-                              radius="m"
-                              sizes={image.width.toString()}
-                              alt={image.alt}
-                              src={image.src}
-                            />
-                          </Flex>
-                        ))}
-                      </Flex>
-                    )}
-                  </Column>
-                ))}
-              </Column>
-            </>
-          )}
-        </Column>
-      </Flex>
-    </Column>
+    </div>
   );
 }
